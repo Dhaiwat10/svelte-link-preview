93158d61e2a01027ba16a3f68f66f1888429ebd8
Magellan <soulmars@live.fr>
2023-08-01 16:04:08 +0200

    set this project up to date

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Preview from './Preview.svelte';
+</script>
+
+<svelte:head>
+	<meta name="description" content="The Climate Game" />
+</svelte:head>
+
+	<Preview />
+
+
---
+++ src/routes/Preview.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { LinkPreview } from '$lib';
+
+</script>
+
+<LinkPreview url="https://www.youtube.com/watch?v=Vjkq8V5rVy0" width="600px" height="400px" proxyUrl="http://localhost:8080" />
+<LinkPreview url="https://barcauniversal.com" proxyUrl="http://localhost:8080" />
